eb038ac8d1c856b681c536a44b4a0c443f3e852d
MMunim <mmunim9.9.01@gmail.com>
2026-08-29 18:29:46 +0600

    get todos by id

--- Module 6/main.py
+++ Module 6/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
 from sqlalchemy.orm import Session
 import models
@@ -22,3 +22,14 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get('/')
 def read_todos(db : db_dependency):
     return db.query(Todos).all()
+
+
+@app.get('/todo/{todo_id}')
+def red_specific_todos(db : db_dependency, todo_id : int):
+    specific_todo = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if specific_todo is not None:
+        return specific_todo
+    else:
+        raise HTTPException(status_code=404, detail='Todo Not Found')
+        
