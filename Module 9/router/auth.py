6d9a6cc516828042556dcc6ec43405793186b375
MMunim <mmunim9.9.01@gmail.com>
2026-09-05 23:25:23 +0600

    fastapi authentication ongoing

---
+++ Module 9/router/auth.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from models import Users
+from fastapi.responses import JSONResponse
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from typing import Annotated
+from database import SessionLocal
+from fastapi.security import OAuth2PasswordRequestForm
+
+router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class CreateUsers(BaseModel):
+    email : str
+    username : str
+    firstname : str
+    lastname : str
+    password : str
+    role : str
+
+
+def authenticate_user(username, password, db):
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if user is None:
+        return False
+    if bcrypt_context.verify(Users.hash_password == password):
+        return True
+    return False
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post('/create_user')
+def create_user(db : db_dependency, new_user : CreateUsers):
+    user_model = Users(
+        email = new_user.email,
+        username = new_user.username,
+        firstname = new_user.firstname,
+        lastname = new_user.lastname,
+        hash_password = bcrypt_context.hash(new_user.password),
+        is_active = True,
+        role = new_user.role
+    )
+
+    db.add(user_model)
+    db.commit()
+
+    return JSONResponse(status_code=201, content={'message' : 'User created successfully!!!'})
+
+
+@router.post('/login')
+def login_user(db : db_dependency, form_data : Annotated[OAuth2PasswordRequestForm, Depends()]):
+
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if user == True:
+        return "Login Successfully!!!"
+    else:
+        return "Invalid Username or Password!!!"
