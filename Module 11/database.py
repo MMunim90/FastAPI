c430df7a82589674fc80ad4c12616d4555787b16
MMunim <mmunim9.9.01@gmail.com>
2026-09-10 12:44:08 +0600

    connecting fastapi project with mysql

--- Module 11/database.py
+++ Module 11/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db' # this is for sqlite database
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:password@localhost/ToDoApplicationDatabase' # this is for postgresql database
+# SQLALCHEMY_DATABASE_URL='postgresql://postgres:password@localhost/ToDoApplicationDatabase' # this is for postgresql database
+
+SQLALCHEMY_DATABASE_URL='mysql+pymysql://root:root@127.0.0.1:3306/todoapplicationdatabase' # this is for mysql database
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
