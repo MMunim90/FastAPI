4f7fcc2043571b98407729f157e774a4dabf4f38
MMunim <mmunim9.9.01@gmail.com>
2026-09-13 21:38:40 +0600

    implement edit profile feature

--- Module 12/router/auth.py
+++ Module 12/router/auth.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from models import Users
 from fastapi.responses import JSONResponse
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from typing import Annotated
+from typing import Annotated, Optional
 from database import SessionLocal
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
@@ -30,6 +30,15 @@ class CreateUsers(BaseModel):
     lastname : str
     password : str
     role : str
+    phone_number : str
+
+
+class UpdateUser(BaseModel):
+    email : Optional[str] = Field(default=None)
+    username : Optional[str] = Field(default=None)
+    firstname : Optional[str] = Field(default=None)
+    lastname : Optional[str] = Field(default=None)
+    phone_number : Optional[str] = Field(default=None)
 
 
 def authenticate_user(username, password, db):
@@ -74,6 +83,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.post('/create_user')
 def create_user(db : db_dependency, new_user : CreateUsers):
@@ -84,7 +94,8 @@ def create_user(db : db_dependency, new_user : CreateUsers):
         lastname = new_user.lastname,
         hash_password = bcrypt_context.hash(new_user.password),
         is_active = True,
-        role = new_user.role
+        role = new_user.role,
+        phone_number = new_user.phone_number
     )
 
     db.add(user_model)
@@ -104,3 +115,21 @@ def login_user(db : db_dependency, form_data : Annotated[OAuth2PasswordRequestFo
     token = create_access_token(user.username, user.id, user.role, timedelta(minutes=30))
 
     return {'access_token' : token, 'token_type' : 'bearer'}
+
+
+
+@router.put('/update_user')
+def update_user(user : user_dependency, db : db_dependency, update_user : UpdateUser):
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="User didnot loged in yet!!!")
+
+    user = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    update_data = update_user.model_dump(exclude_unset=True)
+
+    for key,value in update_data.items():
+        setattr(user,key,value)
+
+    db.commit()
+    return JSONResponse(status_code=200, content={'message' : 'User updated successfully'})
