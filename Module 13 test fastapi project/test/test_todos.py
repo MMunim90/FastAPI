7e1a67092e12c430e34139a6207f1fe9e5e47cd8
MMunim <mmunim9.9.01@gmail.com>
2026-09-22 06:52:36 +0600

    explore authentication override

--- Module 13 test fastapi project/test/test_todos.py
+++ Module 13 test fastapi project/test/test_todos.py
@@ -1,7 +1,16 @@
 from test.test_main import client
 from main import app
 from fastapi import status
+from router.auth import get_current_user
+
+def override_get_current_user():
+    return {
+        'id' : 1,
+        'username' : 'testuser'
+    }
+
+app.dependency_overrides[get_current_user] = override_get_current_user
 
 def test_read_todos():
     response = client.get('/')
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.status_code == status.HTTP_200_OK
